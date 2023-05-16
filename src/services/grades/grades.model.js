80e5074d16bef0bc3ce84a1e550e96315357fd69
kshama <kshamak@valueaddsofttech.com>
2023-05-16 15:14:19 +0530

    tests service removed

--- src/services/grades/grades.model.js
+++
@@ -1,8 +0,0 @@
-import Joi from 'joi'
-const attrs = {
-  name: Joi.string().required(),
-  start: Joi.string().required(),
-  end: Joi.string().required(),
-}
-
-export const gradeSchema = Joi.object(attrs)
