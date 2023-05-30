5a2324e8e715ead902c47ff3ba78c7e644b6e7c3
Swapnil Rajgadkar <129938545+swapnilrajgadkarvast@users.noreply.github.com>
2023-05-30 17:02:43 +0530

    Updated index.js with forgot password and users

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,46 @@
+import { division } from './division/division.js'
+
+import { grades } from './grades/grades.js'
+
+import { report } from './report/report.js'
+
+import { roles } from './roles/roles.js'
+
+import { standard } from './standard/standard.js'
+
+import { student } from './student/student.js'
+
+import { studentTestResult } from './student-test-result/student-test-result.js'
+
+import { subjects } from './subjects/subjects.js'
+
+import { tests } from './tests/tests.js'
+
 import { forgotpassword } from './forgotpassword/forgotpassword.js'
 
 import { user } from './users/users.js'
 
 export const services = (app) => {
+  app.configure(division)
+
+  app.configure(grades)
+
+  app.configure(report)
+
+  app.configure(roles)
+
+  app.configure(standard)
+
+  app.configure(student)
+
+  app.configure(studentTestResult)
+
+  app.configure(subjects)
+
+  app.configure(tests)
+
   app.configure(forgotpassword)
 
   app.configure(user)
-
   // All services will be registered here
 }
