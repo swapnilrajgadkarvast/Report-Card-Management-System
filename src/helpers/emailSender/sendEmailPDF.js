7ee1cefedcfdaf861d9498191986e2efb056cff8
Swapnil Rajgadkar <129938545+swapnilrajgadkarvast@users.noreply.github.com>
2023-06-27 10:52:06 +0530

    Deploying the backend on render

--- src/helpers/emailSender/sendEmailPDF.js
+++ src/helpers/emailSender/sendEmailPDF.js
@@ -1,5 +1,9 @@
 import nodemailer from 'nodemailer'
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 export const sendEmailWithPDF = () => {
   return async (context) => {
     // console.log(context.data)
@@ -15,8 +19,14 @@ export const sendEmailWithPDF = () => {
     // console.log('User Data')
     // console.log(userData)
 
-    console.log('User Data Pdf File Path')
-    console.log(userData.reportFilePath)
+    // console.log('User Data Pdf File Path')
+    // console.log(userData.reportFilePath)
+
+    console.log('Start Timer for 25 second')
+
+    await sleep(25000) // This will sleep process for 30 seconds in that time user will save the pdf file in the specified location
+
+    console.log('Time ended')
 
     // console.log(`the user who just registered is ${userEmail} and data is ${JSON.stringify(userData)}`)
     await main(userEmail, userData, context.data.reportFilePath).catch(console.error)
